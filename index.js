1ffe057fd7fdd8e4e6c6f5031ba84f6af7cb64a2
shoaib soomro <soomroshoaib178@gmail.com>
2024-05-13 07:59:08 -0700

    create two file index.js nd index.html

---
+++ index.js
@@ -0,0 +1,37 @@
+// alert("shoaib soomro ") 
+
+// let name = "shoaib"; variable  var // let 
+// alert(name)
+
+// variable Rules
+
+// legal or ilegal 
+
+// legal 
+// let firstName = "shoaib" camelcase 
+// alert(firstName)
+
+// Allowed special characters  
+// let $namewithdolor = "dolor"
+
+// variable can be not start with number 
+
+// console.log("helloworlds")
+
+// data type 
+
+// String
+//let firstName = "shoaib "
+// any latter word or sentence is string in javascript
+//  Number Boolean 
+
+let num = "9";
+
+alert(typeof(num))
+
+// git init 
+// git add indexedDB.js
+// git def   hum new file konse add kar rha hai
+// git log ---------------------------------commit  histry
+// git log --oneline
+// git show past id
